7ef1f3f29c07affbb40208c451c776432b2ff640
jingizoo <jalaj.sm@gmail.com>
2016-07-24 23:20:23 +0530

    baseline1

---
+++ app/src/main/java/com/jalaj/firstapp/todolist/adapter/NoteListAdapter.java
@@ -0,0 +1,8 @@
+package com.jalaj.firstapp.todolist.adapter;
+
+/**
+ * Created by jalajmehta on 7/24/16.
+ */
+
+public class NoteListAdapter {
+}
---
+++ app/src/main/java/com/jalaj/firstapp/todolist/model/ToDoListItem.java
@@ -0,0 +1,12 @@
+package com.jalaj.firstapp.todolist.model;
+
+/**
+ * Created by jalajmehta on 7/24/16.
+ */
+
+public class ToDoListItem {
+    String noteContent;
+    String reminder;
+    Boolean isDone;
+
+}
